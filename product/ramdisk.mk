315f878dd5b8ca3734266d1af35e704e33fb7f9b
Joe <joe2k01dev@gmail.com>
2017-05-30 19:34:48 +0200

    mt6737X-common: fix, fstab and init are the same for all the mt67xx platforms more or less

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,4 +1,4 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
-	$(LOCAL_PATH)/rootdir/etc/fstab.$(TARGET_BOARD_PLATFORM):root/fstab.$(TARGET_BOARD_PLATFORM) \
-	$(LOCAL_PATH)/rootdir/etc/init.recovery.$(TARGET_BOARD_PLATFORM).rc:root/init.recovery.$(TARGET_BOARD_PLATFORM)
+	$(LOCAL_PATH)/rootdir/etc/fstab.mt6737X:root/fstab.$(TARGET_BOARD_PLATFORM) \
+	$(LOCAL_PATH)/rootdir/etc/init.recovery.mt6737X.rc:root/init.recovery.$(TARGET_BOARD_PLATFORM)
